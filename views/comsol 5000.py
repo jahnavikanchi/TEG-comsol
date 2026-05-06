bb3d6ec009524b9227e41fed82728f34bf6de871
Jahnavi <jahnavikanchi3@gmail.com>
2026-05-06 17:56:07 +0200

    Uploading views

---
+++ views/comsol 5000.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import base64
+
+
+st.markdown('''
+    <style>
+    body {
+        color: white;  /* Text color */
+        font-family: 'Times New Roman', sans-serif;  /* Default font */
+    }
+    h1 {
+        font-family: 'Georgia', sans-serif;  /* Different font for headers */
+        color: white; font-size: 50px; 
+    }
+    h2, {
+        font-family: 'Times New Roman', monospace;  /* Different font for headers */
+        color: white; font-size: 50px;
+    }
+    .member-name {
+        font-family: 'Times New Roman', serif;  /* Font for member names */
+        font-size: 14px;  /* Size for member names */
+    }
+   .member-role {
+        font-family: 'Times New Roman', sans-serif;  /* Font for member roles */
+        font-size: 12px;  /* Size for member roles */
+    }
+    </style>
+    ''', unsafe_allow_html=True)
+
+st.write("<h1>5000 Cases Simulation</h1>", unsafe_allow_html=True)
+st.write("<h2>1. Generating case parameters</h2>", unsafe_allow_html=True)
+
+st.write("The parameters such as the height of the thermoelectric leg, the height of the interconnects, hot side temperature, etc... were generated using python." \
+"The parameter values were generated based on specific criteria and subjected to predefined constraints to ensure validity and relevance. These values were then stored in a CSV file, which serves as the foundation for constructing the dataset used in subsequent analysis or modeling. [[Colab File]](https://colab.research.google.com/drive/1DfcdirBFgdQxCMptFk3BuDq6BVCpin_b?usp=sharing)" )
+
+st.write("")
+
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def load_image(image_file):
+    with open(image_file, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+
+hist_path = "views/Images/histogram.png"
+image_base64 = load_image(hist_path)
+
+if image_base64:
+    st.markdown(f'<img src="data:image/jpeg;base64,{image_base64}" alt="Image" style="width:100%;">', unsafe_allow_html=True)
+
+st.markdown("<h3 style='text-align: center;font-size: 16px;'>Fig 1: The histogram depicts the value spread of the parameters data generated.The x-axis represents the parameter values, while the y-axis indicates the frequency of each value. This visualization provides insights into the range and distribution of the generated parameters, helping to identify any potential biases or anomalies in the data generation process.</h3>", unsafe_allow_html=True)
+
+
+st.write("<h2>2. Finding a polynomial as a function of temperature</h2>", unsafe_allow_html=True)
+
+st.write("The polynomial function was generated using the numpy library in Python. The coefficients of the polynomial were determined through a fitting process. The generated polynomial function can be used for further analysis or modeling tasks. [[Colab File]](https://colab.research.google.com/drive/1GrvoyQqBbBhPa4e7cMgTDY4QXbsX4PPH?usp=sharing)" )
+
+st.write("<h2>3. (a) Simulating each case: Batch Sweep</h2>", unsafe_allow_html=True)
+
+st.write("The simulation was performed using the COMSOL Multiphysics software. It was found that there were different ways to go about simulating 5K differnt cases, first one being parametric sweep in one comsol file, second is using Matlab, and the last is using simple python." \
+"\nWe chose the last option which is using python. For this partcular method, it's required to generate a .txt file for each input, hence using another python code, generated about 5000  geometry case files, gave them as input and simulated each case, generating .mph file for each.\n"
+)
+
+st.write("So, the first step is to create a template file with the respective variables set in the global parameters and initialise them with a random value, and simulate it, and then use this as a template file for the 5k cases.\n" \
+"The example .txt file is uploaded in the loop folder of the repositary. The python code used for the simulations has also been uploaded into the loop folder." \
+"For each case it took about 50sec of simulation time. So, total simulation time took about 39 hrs." \
+"", unsafe_allow_html=True)
+
+
+st.write("\nIt was after the simulation we realised the the export of each case is not being done, but each case generated had a .mph file respective to it's parameters and the computation has been performed successfully.")
+
+st.write("So, at this point we decided to utilise matlab to extract the results from each .mph file, as it's mentioned that comsol files can be loaded in matlab via COMSOL LiveLink")
+
+
+st.write("<h2>3. (b) Simulating each case: Parametric Sweep</h2>", unsafe_allow_html=True)
+
+st.write("After a failed attemot at extracting the results from the .mph files, we decided to use the parametric sweep method in comsol. The parametric sweep method allows us to define a range of values for a parameter sucha as the width, height of TE and automatically run simulations for each value in that range. This approach is particularly useful when dealing with a large number of cases, as it eliminates the need to manually create and manage individual simulation files for each case.\n" )
+st.write("The arametric sweep comes under the study node in comsol, and we can define the parameters to sweep over, and then run the simulation. The results can be exported to a file for further analysis.\n The probes were defined at the ohmic contacts to measure the voltage generated for the specific cominations." )
+st.write("It took about 24 hours for the parametric sweep to complete, and the results were exported to a .csv file for further analysis. The results were then analysed using python.\n")
+
